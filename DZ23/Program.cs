efab770b21c4a4b3562eea64c6570581aa0b9e8a
Dmitry <dep1982@mail.ru>
2022-10-15 20:34:24 +0300

    Zadacha23

---
+++ DZ23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+int InNumber()
+{
+    while (true)
+    {
+        string text = Console.ReadLine();
+        if (int.TryParse(text, out int number))
+        {
+            return number;
+        }
+        else
+        {
+            Console.Write("Не удалось распознать число. \nВведите число ещё раз: ");
+        }
+    }
+}
+
+Console.Write("Введите число: ");
+
+int cube = InNumber();
+
+for(int i =1; i <= cube; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)} ");
+
+}
